ba362d81632570f9099a344d6e4714098bbe75ea
santiago <santiago.pelaez@wsu.edu>
2026-04-11 13:18:15 -0700

    Added all the new stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
+// #include <Arduino.h>
+
+// // put function declarations here:
+// int myFunction(int, int);
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   int result = myFunction(2, 3);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
 #include <Arduino.h>
+#define LED_PIN 15
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  Serial.println("Setup done");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+  Serial.println("Looping...");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// GPIO 25, 26, 33
+// Word Select, Serial Clock, Serial Data
