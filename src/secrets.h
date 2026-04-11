f56cb071d6ec183b58572074197b5fe0d5be8925
santiago <santiago.pelaez@wsu.edu>
2026-04-11 14:49:09 -0700

    IDK what this is im sorry it's so messy

---
+++ src/secrets.h
@@ -0,0 +1,14 @@
+// src/secrets.h
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// ===== BLYNK IoT =====
+#define BLYNK_TEMPLATE_ID "TMPL26PleId9c"
+#define BLYNK_TEMPLATE_NAME "LED Text"
+#define BLYNK_AUTH_TOKEN "sVOI8SxxY5HdtJzS-j2bUYnA8ycil2Ms"
+
+// ===== Wi-Fi =====
+const char *ssid = "iPhone";
+const char *pass = "12345678";
+
+#endif
